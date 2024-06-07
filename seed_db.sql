fedf08f34a2754d0c8098738197ab14ba2b8d041
Christopher Patty <christopher.jon.patty@gmail.com>
2024-06-06 18:31:26 -0600

    Update seed_db.sql

--- seed_db.sql
+++ seed_db.sql
@@ -1,2 +1,14 @@
 -- Here you can add any sql you want such as queries to create tables, insert data, etc.
 -- The file will only be executed when you change anything in it and will run only once.
+
+-- Create a table with animals
+CREATE TABLE animals (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
+
+-- Insert some trains
+INSERT INTO animals (id, name) VALUES (1, 'Giraffe');
+INSERT INTO animals (id, name) VALUES (2, 'Kangaroo');
+INSERT INTO animals (id, name) VALUES (3, 'Dolphin');
+INSERT INTO animals (id, name) VALUES (4, 'Cat');
